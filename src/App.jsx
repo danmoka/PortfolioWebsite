adab5cd8357eed634951a9adf9a66d8e700da862
Daniil Potemkin <daniilpot@live.com>
2022-05-19 00:00:14 +0400

    Add basic components and snap scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/Intro";
+import Topbar from "./components/topbar/Topbar";
+import "./app.scss";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div className="app">
+      <Topbar />
+      <div className="sections">
+        <Intro />
+        <Contact />
+      </div>
+    </div>
+  );
 };
 
 export default App;
