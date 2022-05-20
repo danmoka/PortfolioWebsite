8ba912a28ebdf0b808744d246be2b8eccbae75e1
Daniil Potemkin <daniilpot@live.com>
2022-05-20 14:33:08 +0400

    Add topbar items

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
 import Topbar from "./components/topbar/Topbar";
 import "./app.scss";
 
 const App = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Contact />
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,28 @@
+import { ContactPhone, Mail } from "@material-ui/icons";
 import "./topbar.scss";
 
-const Topbar = () => {
+const Topbar = ({ menuOpen, setMenuOpen }) => {
     return (
-        <div className="topbar">
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">windmill.</a>
+                    <div className="itemContainer">
+                        <ContactPhone className="icon"/>
+                        <span>+7 999 123 12 50</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>hr@windmill.com</span>
+                    </div>
                 </div>
-                <div className="right"></div>
+                <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
             </div>
         </div>
     );
